c260993e3cf39a317c64ef7cd3ba1b0458d37f03
sonalgaud12 <82362774+sonalgaud12@users.noreply.github.com>
2024-01-16 20:30:15 +0530

    Update 0380-insert-delete-getrandom-o1.java

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,10 +1,17 @@
+/*
+Question link: https://leetcode.com/problems/insert-delete-getrandom-o1/
+Time Complexity: O(1)
+Space Complexity: O(1)
+
+*/
+
 class RandomizedSet {
     
     private ArrayList<Integer> list;
     private HashMap<Integer, Integer> map;
     private Random random;
-    
 
+    // Initialize your data structure here.
     public RandomizedSet() {
         
         list = new ArrayList<>();
@@ -12,7 +19,8 @@ class RandomizedSet {
         random = new Random();
         
     }
-    
+
+    // Inserting a value to the list. Returns true if the list did not already contain the specified element.
     public boolean insert(int value) {
          if (map.containsKey(value)) {
             return false;
@@ -22,7 +30,8 @@ class RandomizedSet {
         return true;
         
     }
-    
+
+    // Removing a value from the list. Returns true if the list contained the specified element
     public boolean remove(int value) {
         if (!map.containsKey(value)) {
             return false;
@@ -36,7 +45,7 @@ class RandomizedSet {
         return true;
         
     }
-    
+    // getting a random element from the list
     public int getRandom() {
         
         int index = random.nextInt(list.size());
@@ -45,10 +54,20 @@ class RandomizedSet {
     }
 }
 
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
+/*
+Question Breakdown: Simply the question states that there is an RandomizedSet() Initializes the RandomizedSet object.
+There are different methods given like bool insert(int value),bool remove(int value),int getRandom()
+In the *insert method* we have to write the code in such a say if a number is present then return false, 
+if not present then insert and return true.
+In *remove method* bascially checking if the number present,if there then remove and return true else false
+In *getRandom* we have to return any random number which is present in the set
+
+Approach: As the main challenge comes is that we need to write code in O(1).
+If we approach it through vector the insert and getRandom method would work in O(1) 
+but the remove method take O(n).
+So the another approach we will go is to map( cause we can store the element with index in map)
+so we will set an set and a map to store the numbers.
+
+Then we will write operations according to the need
+
+*/
