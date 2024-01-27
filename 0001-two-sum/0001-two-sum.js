3bfa3d285368f10d6d2e46b291dcff9d9376ea16
sonalgaud12 <82362774+sonalgaud12@users.noreply.github.com>
2024-01-27 16:34:58 +0530

    Update 0001-two-sum.js

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,24 +3,38 @@ Time Complexity: O(n)
 Space COmplexity: O(1)
  */
 
-
+// Javascript
 var twoSum = function(nums, target) {
     
-    let mp = new Map()
-    
+    let mp = new Map() // creating a map
+    // starting a iteration 
     for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i]
+        let diff = target - nums[i] // declaring difference target - nums of ith index
+        
         
         if (mp.has(diff)) {
-            return [i, mp.get(diff)]
+            return [i, mp.get(diff)] // if the condition matches then returning in to map
         }
-        
+        // stores the value by the key
         mp.set(nums[i], i)
     }
     
 };
-
+// Java
+Map<Integer, Integer> num_map = new HashMap<>();
+        
+        for(int i=0; i<nums.length; i++){
+            int complement = target - nums[i];
+            
+            if(num_map.containsKey(complement)) {
+                return new int[] {num_map.get(complement), i};
+                
+            }
+            num_map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("no match found!");
 /*
+
 Question Breakdown: Given an array of integers nums and an integer target 
 we just need to find two numbers prsent in the array which comes to target
 
