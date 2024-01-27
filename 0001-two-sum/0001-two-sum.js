3eaacb4b98c1611af85a20e25d1aba3a0e1f4d65
sonalgaud12 <82362774+sonalgaud12@users.noreply.github.com>
2024-01-27 16:31:05 +0530

    Update 0001-two-sum.js

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,8 +1,9 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+/* 
+Time Complexity: O(n)
+Space COmplexity: O(1)
  */
+
+
 var twoSum = function(nums, target) {
     
     let mp = new Map()
@@ -18,3 +19,14 @@ var twoSum = function(nums, target) {
     }
     
 };
+
+/*
+Question Breakdown: Given an array of integers nums and an integer target 
+we just need to find two numbers prsent in the array which comes to target
+
+Approach: Using the brute force approach we could just keep two variables but that time complexity would be O(n^2)
+to optimise the solution we can just use hashmap
+In which it will store the value with index 
+and we will create a difference that will be target - num
+and if map has the difference values then we will put the ith index and rhe difference in the map
+*/
